cd365cd9f821ccf1f44e6907c53c54ae03ed71d0
sdhanush-git <medhanush100@gmail.com>
2025-08-11 19:54:53 +0530

    i will add card and fetch movies lists

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,11 +13,11 @@ const Header = () => {
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <NavLink
             to="https://flowbite.com/"
-            className="flex items-center space-x-3 rtl:space-x-reverse"
+            className="flex items-center space-x-3 rtl:space-x-reverse text-white"
           >
             <img
               src="https://flowbite.com/docs/images/logo.svg"
-              className="h-8"
+              className="h-8 mr-4 "
               alt="Flowbite Logo"
             />
             Cinemabite
--- src/pages/MoviesList.jsx
+++ src/pages/MoviesList.jsx
@@ -1,5 +1,50 @@
+import { useState, useEffect } from "react";
+import Card from "../components/Card";
+
 const MoviesList = () => {
-  return <main>MoviesList</main>;
+
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMzQ0ZmQ2OWZlNTgzYzk5ZGFhMTM3NDVmZjUwN2U2NSIsIm5iZiI6MTc1NDg5NDQ1Ni43NDQsInN1YiI6IjY4OTk5MDc4ODlmYThiMGEwNTI2N2VkOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.sI3MH08Bvwf1oUUMziOcNCyOnnbFDQnA0TaLXcVDwQU",
+    },
+  };
+
+  const dummyMovies = new Array(6).fill(null)
+
+  const [movies, setMovies] = useState([])
+
+  useEffect(()=>{
+    async function fetchMovies() {
+      const response = await fetch(
+        "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1",
+        options
+      );
+
+      const data = await response.json();
+
+      setMovies(data.results);
+
+    }
+    fetchMovies();
+
+},[])
+
+  return (
+    <main>
+      <section className="max-w-7xl max-auto py-7">
+        <div className="grid gap-5 sm:grid-cols-2 lg:grid-cols-3">
+          {
+          dummyMovies.map((_, index) =>(
+            <Card key={index}/>
+          ))
+          }
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default MoviesList;
